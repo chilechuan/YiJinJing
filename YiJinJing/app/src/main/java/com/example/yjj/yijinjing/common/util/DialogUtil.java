ab1c9f105ede3183856b2afb2efde0d2444a0d55
tongxin <2954676638@qq.com>
2017-11-15 18:21:56 +0800

    dialog第一次上传22222

--- YiJinJing/app/src/main/java/com/example/yjj/yijinjing/common/util/DialogUtil.java
+++ YiJinJing/app/src/main/java/com/example/yjj/yijinjing/common/util/DialogUtil.java
@@ -1,5 +1,8 @@
 package com.example.yjj.yijinjing.common.util;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * Created by tx on 2017/11/15.
  */
@@ -8,6 +11,10 @@ public class DialogUtil {
 
     public DialogUtil(){
 
+    }
+
+    public static void CreateDialog(Context context){
+        Toast.makeText(context, "哈哈", Toast.LENGTH_SHORT).show();
     }
 
 }
